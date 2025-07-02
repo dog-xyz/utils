7444d6fd5df42a555dff7c910d9a6a409bcb6bd6
watchman1989 <alonealienware@gmail.com>
2025-07-02 21:49:24 +0800

    add floor ticker

--- cron/floor_ticker_test.go
+++ cron/floor_ticker_test.go
@@ -13,11 +13,8 @@ func TestFloorTicker(t *testing.T) {
 	defer ft.Stop()
 
 	go func() {
-		for {
-			select {
-			case <-ft.C:
-				t.Logf("tick at: %s, last: %s\n", time.Unix(ft.CurrTs, 0).Format("2006-01-02 15:04:05"), time.Unix(ft.LastTs, 0).Format("2006-01-02 15:04:05"))
-			}
+		for range ft.C {
+			t.Logf("tick at: %s, last: %s\n", time.Unix(ft.CurrTs, 0).Format("2006-01-02 15:04:05"), time.Unix(ft.LastTs, 0).Format("2006-01-02 15:04:05"))
 		}
 	}()
 
